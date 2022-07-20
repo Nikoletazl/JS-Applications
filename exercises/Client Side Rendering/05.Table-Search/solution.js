10907c497d53e9818d737f13fb29416084e975fc
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-07-20 16:15:29 +0300

    Add files via upload

---
+++ exercises/Client Side Rendering/05.Table-Search/solution.js
@@ -0,0 +1,12 @@
+import {render} from "../node_modules/lit-html/lit-html.js"
+import {getAllStudents} from './api.js'
+import {studentsTemplate} from './students.js'
+import {solve} from './search.js'
+
+let tableBody = document.querySelector('.container tbody')
+let studentsData = await getAllStudents()
+let template = studentsTemplate(Object.values(studentsData))
+
+render(template, tableBody)
+let searchBtn = document.querySelector('#searchBtn')
+searchBtn.addEventListener('click', solve)
