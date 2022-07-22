4bad893df2efb156a53a49e8da3cd7864b1eb003
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-07-22 21:49:24 +0300

    Add files via upload

---
+++ exercises/Routing/01.Furniture/views/logout.js
@@ -0,0 +1,16 @@
+import {updateInfo} from '../app.js'
+import page from '../node_modules/page/page.mjs'
+
+export const logout = () => {
+    fetch('http://localhost:3030/users/logout', {
+        method: 'get',
+        headers: {
+            'X-Authorization': localStorage.token
+        }
+    })
+
+    localStorage.clear()
+    page.redirect('/catalog')
+    updateInfo()
+
+}
